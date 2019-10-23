27de20cce5761b286ba622e89c677f779564ee11
arda000 <56915096+arda000@users.noreply.github.com>
2019-10-23 12:32:57 +0300

    Update HelloGitHub.java

--- Java/HelloGitHub.java
+++ Java/HelloGitHub.java
@@ -9,3 +9,14 @@ public class HelloGitHub {
 	}
 
 }
+public class HelloGitHub {
+
+	public static void main(String[] args) {
+
+	    System.out.println("Hello, GitHub!");
+	    
+	    //Yukarıdaki komutun sözdizimini kullanarak aşağıya "Hello, GitHub!" yazdırın.
+
+	}
+
+}
